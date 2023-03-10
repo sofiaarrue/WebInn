1124a4c324687ebf6eb287254479810858ea0f94
sofiaarrue <sofiarruee@gmail.com>
2023-03-10 16:36:08 -0300

    cambios

--- MansoWeb/js/menu.js
+++ MansoWeb/js/menu.js
@@ -4,5 +4,3 @@ const navbarUlElement = document.getElementById("navbar-ul");
 toggleMenuElement.addEventListener("click", () => {
     navbarUlElement.classList.toggle("navbar-ul--show");
 });
-
-
--- MansoWeb/js/sliders.js
+++ MansoWeb/js/sliders.js
@@ -1,4 +1,22 @@
 
-const sliders = [...document.querySelectorAll(".slider__body")];
-const arrowNext = document.querySelector("#next");
-const arrowBefore = document.querySelector("#before");
+const sliders = [...document.getElementsByClassName("slider__body")];
+const arrowNext = document.getElementById("next");
+const arrowBefore = document.getElementById("before");
+let value;
+
+function changePosition(change){
+    const currentElement = Number(document.getElementsByClassName("slider__body--show").detaset.id);
+
+    value = currentElement;
+    value+=change;
+
+    if (value === 0 || value == sliders.length+1) {
+        value = value === 0 ? sliders.length : 1;
+    }
+
+    sliders[currentElemnt-1].classList.toggle("slider__body--show");
+    sliders[value-1].classList.toggle("slider__body--show");
+}
+
+arrowNext.addEventListener("click", () => changePosition(1));
+arrowBefore.addEventListener("click", () => changePosition(-1));
