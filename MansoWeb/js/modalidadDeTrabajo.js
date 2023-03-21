113741caf93d62f769b0b6b5e144500a1935f0c9
sofiaarrue <sofiarruee@gmail.com>
2023-03-21 12:54:49 -0300

    cambios

--- MansoWeb/js/modalidadDeTrabajo.js
+++ MansoWeb/js/modalidadDeTrabajo.js
@@ -1,14 +1,14 @@
-const toggleDown = document.querySelectorAll(".toggle-down");
-const toggleUp = document.querySelectorAll(".toggle-up");
-const pasosTitle = document.querySelectorAll(".pasosTitle");
-const pasosP = document.querySelectorAll(".pasosP");
+const toggleDown = [...document.querySelectorAll("toggle-down")];
+const toggleUp = [...document.querySelectorAll("toggle-up")];
+const pasosTitle = [...document.querySelectorAll("pasosTitle")];
+const pasosP = [...document.querySelectorAll("pasosP")];
 
-toggleDown.addEventListener("click", () => {
-    pasosP.classList.toggle("pasosP--show");
-    pasosTitle.classList.toggle("pasosTitle--hidden");
-});
+toggleDown.forEach(element => ("click", () => {
+        pasosP.classList.add("pasosP--show");
+        pasosTitle.classList.add("pasosTitle--hidden")
+}));
 
 toggleUp.removeEventListener("click", () => {
     pasosP.classList.toggle("pasosP--show");
-    pasosTitle.classList.toggle("pasosTitle--hidden");
+    pasosTitle.classList.toggle("pasosTitle--hidden")
 });
