e4d391241d581cbbec85a3884a6560a62732bf5e
sofiaarrue <103455609+sofiaarrue@users.noreply.github.com>
2024-10-09 10:21:15 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -54,7 +54,7 @@
                 </div>
                 <div class="col-12 col-md-8 my-auto text-center">
                     <p><a href="https://api.whatsapp.com/send?phone=91122627582" target="_blank" style="text-decoration: none; color: #fff;"><img src="imagenes/whatsapp.svg"><b>Tel:</b> +54 9 11 2262-7582</p></a>
-                    <p><a href="mailto:contacto@flexywebs.com" target="_blank" style="text-decoration: none; color: #fff;"><i class="fa-solid fa-envelope footer__i p-1"></i> <b>Email: </b>contacto@flexywebs.com</p></a>
+                    <p><a href="mailto:contacto@flexyweb.com.ar" target="_blank" style="text-decoration: none; color: #fff;"><i class="fa-solid fa-envelope footer__i p-1"></i> <b>Email: </b>contacto@flexyweb.com.ar</p></a>
                 </div>
             </div>
         </div>
